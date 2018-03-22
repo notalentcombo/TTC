75337dadceeb4e08fc9cac942642f9f66df9855a
notalentcombo <notalentcombo@gmail.com>
2018-03-22 01:34:22 -0400

    More Setup

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController> {
+
+    protected GameController() {}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TTC
 Chess meets Tic Tac Toe
+
+Art from https://opengameart.org/content/boardgame-tiles (thanks Sharm!)
